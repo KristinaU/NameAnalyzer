c5b970bed7cc9c766e99a83b2d81647eb1c03a3e
Kristina Urosova <urosova.k@gmail.com>
2023-01-04 16:25:08 +0000

    Initial Commit

---
+++ NameAnalyzer/NameAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameAnalyzerApp.swift
+//  NameAnalyzer
+//
+//  Created by KRISTINA UROSOVA on 04/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NameAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
